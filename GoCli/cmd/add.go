a51f3daac955949b3e41a269b3de3ffaa12bc027
Sarvjeet Rajvansh <rajvanshsarvjeet@gmail.com>
2025-07-11 00:00:03 +0530

    Day2

--- GoCli/cmd/add.go
+++ GoCli/cmd/add.go
@@ -1,16 +1,18 @@
 /*
-Copyright © 2025 SARVJEET RAJVANSH social.sarvjeetrajvansh@gmail.com
+Copyright © 2025 NAME HERE <EMAIL ADDRESS>
+
 */
 package cmd
 
 import (
 	"fmt"
+
 	"github.com/spf13/cobra"
 )
 
-// helloCmd represents the hello command
-var helloCmd = &cobra.Command{
-	Use:   "hello",
+// addCmd represents the add command
+var addCmd = &cobra.Command{
+	Use:   "add",
 	Short: "A brief description of your command",
 	Long: `A longer description that spans multiple lines and likely contains examples
 and usage of using your command. For example:
@@ -19,23 +21,20 @@ Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("hello called")
-		name, _ := cmd.Flags().GetString("name")
-		fmt.Printf("Hello, %s!\n", name)
+		fmt.Println("add called")
 	},
 }
 
 func init() {
-	rootCmd.AddCommand(helloCmd)
-	helloCmd.Flags().StringP("name", "n", "world", "Name to greet")
+	rootCmd.AddCommand(addCmd)
 
 	// Here you will define your flags and configuration settings.
 
 	// Cobra supports Persistent Flags which will work for this command
 	// and all subcommands, e.g.:
-	// helloCmd.PersistentFlags().String("foo", "", "A help for foo")
+	// addCmd.PersistentFlags().String("foo", "", "A help for foo")
 
 	// Cobra supports local flags which will only run when this command
 	// is called directly, e.g.:
-	// helloCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	// addCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
